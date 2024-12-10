92a9795555153e26b80012e63ff8f54c71ad776b
hamzael69 <elomarihamza835@gmail.com>
2024-12-10 16:58:10 +0100

    exo 2

---
+++ ajout-patient.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom">
+
+        <label for="prenom">Prenom : </label>
+        <input type="text" name="prenom" id="prenom">
+
+        <label for="birthdate">Date de naissance : </label>
+        <input type="text" name="birthdate" id="nom">
+
+        <label for="phone">Tel : </label>
+        <input type="text" name="phone" id="phone">
+
+        <label for="mail">Mail : </label>
+        <input type="text" name="mail" id="mail">
+        
+
+
+        <input type="submit" value="Creer patient">
+    </form>
+
+
+</body>
+</html>
